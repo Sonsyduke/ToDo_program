e4e2ed90aee64ba8ac4d4a9760ca95238a0f52a2
Douglas <douglaslombard29@gmail.com>
2022-03-22 18:40:37 +0200

    looping through array and adding list items to the DOM

--- js/app.js
+++ js/app.js
@@ -3,15 +3,31 @@ const button = document.querySelector(".submit");
 const todos = document.querySelector(".todos");
 const heading = document.querySelector("#main-heading");
 
-button.addEventListener("click", function () {
-  const item = document.createElement("li");
-  item.innerText = input.value;
-  todos.appendChild(item);
-  input.value = "";
-  item.addEventListener("click", function () {
-    item.style.textDecoration = "line-through";
-  });
-  item.addEventListener("dblclick", function () {
-    todos.removeChild(item);
-  });
-});
+// button.addEventListener("click", function () {
+//   const item = document.createElement("li");
+//   item.innerText = input.value;
+//   todos.appendChild(item);
+//   input.value = "";
+//   item.addEventListener("click", function () {
+//     item.style.textDecoration = "line-through";
+//   });
+//   item.addEventListener("dblclick", function () {
+//     todos.removeChild(item);
+//   });
+// });
+
+// BASIC TODO LIST PROGRAM
+
+// =======================================
+
+const listOfToDos = ["Washing the dog", "Training", "Cleaning"];
+
+function todosItems(arr) {
+  let items = "";
+  for (let i = 0; i < arr.length; i++) {
+    items += `<li>${arr[i]}</li>`;
+  }
+  return items;
+}
+
+todos.innerHTML = todosItems(listOfToDos);
