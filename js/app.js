a0606045d31b429a70f9b7034ebb5a7bd5453e6c
Douglas <douglaslombard29@gmail.com>
2022-03-22 17:11:42 +0200

    Basic logic complete

--- js/app.js
+++ js/app.js
@@ -1,13 +1,17 @@
-const tasks = [
-  "Washing the dog",
-  "Finishing my Portfolio",
-  "Finishing the CSS course",
-];
-
-const input = document.querySelector("#input-field").value;
+const input = document.querySelector("#input-field");
 const button = document.querySelector(".submit");
-const lists = document.querySelector(".items");
+const todos = document.querySelector(".todos");
+const heading = document.querySelector("#main-heading");
 
-for (let i = 0; i < tasks.length; i++) {
-  lists.innerHTML = `<li>${tasks[i]}</li>`;
-}
+button.addEventListener("click", function () {
+  const item = document.createElement("li");
+  item.innerText = input.value;
+  todos.appendChild(item);
+  input.value = "";
+  item.addEventListener("click", function () {
+    item.style.textDecoration = "line-through";
+  });
+  item.addEventListener("dblclick", function () {
+    todos.removeChild(item);
+  });
+});
