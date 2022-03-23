e4c7ca23220b5df07e4419a1692e5d47e6545177
Douglas <douglaslombard29@gmail.com>
2022-03-23 09:12:57 +0200

    Finished the logic for the todo program

--- js/app.js
+++ js/app.js
@@ -2,26 +2,12 @@ const input = document.querySelector("#input-field");
 const button = document.querySelector(".submit");
 const todos = document.querySelector(".todos");
 const heading = document.querySelector("#main-heading");
-
-// button.addEventListener("click", function () {
-//   const item = document.createElement("li");
-//   item.innerText = input.value;
-//   todos.appendChild(item);
-//   input.value = "";
-//   item.addEventListener("click", function () {
-//     item.style.textDecoration = "line-through";
-//   });
-//   item.addEventListener("dblclick", function () {
-//     todos.removeChild(item);
-//   });
-// });
-
-// BASIC TODO LIST PROGRAM
-
-// =======================================
+const deleteBtnF = document.querySelector("#deleteFirst");
+const deleteBtnL = document.querySelector("#deleteLast");
 
 const listOfToDos = [];
 
+// Function to Loop through the array
 function todosItems(arr) {
   let items = "";
   for (let i = 0; i < arr.length; i++) {
@@ -30,9 +16,22 @@ function todosItems(arr) {
   return items;
 }
 
+// Event listener to add new to dos to the list
 button.addEventListener("click", function () {
   const item = input.value;
   listOfToDos.push(item);
   input.value = "";
   todos.innerHTML = todosItems(listOfToDos);
+});
+
+// Event listener to remove the first list item
+deleteBtnF.addEventListener("click", function () {
+  listOfToDos.shift();
+  todos.removeChild(todos.firstChild);
+});
+
+// Event listener to remove the last list item
+deleteBtnL.addEventListener("click", function () {
+  listOfToDos.pop();
+  todos.removeChild(todos.lastChild);
 });
